81fea675b8883b16cc7306dbcf1e0ccb13f2c931
in_esp <in_esp@elorrieta.int>
2025-09-30 10:25:37 +0200

    limpiada la logica

--- Algoritmia/Iniciales/ActividadPIN/main.py
+++ Algoritmia/Iniciales/ActividadPIN/main.py
@@ -1,25 +1,37 @@
+# Input con validación para integers
+"""
 def validate_int(data: str) -> int:
     try:
         return int(input(data))
     except ValueError:
         print('El valor ingresado no es valido, debe ser un numero')
         return -1
+"""
 
 
+def validate_int(data: str) -> int | None:
+    str_input: str = input(data)
+    if str_input.isdigit():
+        return int(str_input)
+    print('El valor ingresado no es valido, debe ser un numero')
+    return None
+
+
+# Variables
 correct_pin: int = 1234
 tries: int = 3
 
-while True:
-    pin: int = validate_int('Ingrese un número: ')
-
+# Bucle de intentos de acceso
+while tries > 0:
     tries -= 1
-    if tries <= 0:
-        print('Te quedaste sin intentos')
-        break
+
+    pin: int = validate_int('Ingrese un número: ')
 
     if pin == correct_pin:
         print('PIN Correcto')
         break
 
-    print(f'Pin incorrecto, te quedan {tries} intentos')
-    print(' ')
+    print(f'Pin incorrecto, te quedan {tries} intentos\n')
+
+else:
+    print('Te quedaste sin intentos')
